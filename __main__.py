3d23f8af52deff8d48011475c11a995e232811b4
XantX <hp_xantx@hotmail.com>
2022-03-15 17:28:27 -0500

    Inicializacion de la interfac grafica

--- __main__.py
+++ __main__.py
@@ -1,8 +1,16 @@
 import tkinter as tk
+import config.restaurante_datos as rest_data
 
 
 def main():
     root = tk.Tk()
+    root.title(rest_data.nombre)
+    root.iconbitmap(rest_data.ico)
+    root.resizable(0, 0)
+    frame = tk.Frame(root)
+    frame.pack()
+    frame.config(width=480, height=400, bg='green')
+
     root.mainloop()
 
 
---
+++ config/restaurante_datos.py
@@ -0,0 +1,5 @@
+nombre = "Restaurante pepes"
+descripcion = """Restaurante de ejemplo para el
+                desarrollo de la aplicacion de restaurante"""
+dueno = "pepe"
+ico = "config/img/restaurante.ico"
